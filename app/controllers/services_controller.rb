4d2a07906fa77af30d054c03925cb5398965cc1a
William Rocha <wiilliamdroocha@gmail.com>
2024-07-14 19:13:57 +0100

    atualização

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,16 @@
-class ServicesController < ApplicationController
-  def index
+class PagesController < ApplicationController
+  def services
+  end
+
+  def open_company
+  end
+
+  def tax_advice
+  end
+
+  def legal_support
+  end
+
+  def financial_planning
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  get 'services', to: 'pages#services'
-  get 'about', to: 'pages#about'
+  scope 'services' do
+    get '/', to: 'pages#services', as: :services
+    get 'open_company', to: 'pages#open_company', as: :services_open_company
+    get 'tax_advice', to: 'pages#tax_advice', as: :services_tax_advice
+    get 'legal_support', to: 'pages#legal_support', as: :services_legal_support
+    get 'financial_planning', to: 'pages#financial_planning', as: :services_financial_planning
+  end
+
+  get 'about', to: 'pages#about', as: :about
 
   resources :blogs
 end
